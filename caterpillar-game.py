d9af85e61bbb8efeaad80b86e346ca062bb320f0
username <54479676+CormacKrum@users.noreply.github.com>
2020-12-05 18:04:56 +0530

    Initial Commit

--- caterpillar-game.py
+++ caterpillar-game.py
@@ -4,12 +4,6 @@ import random as rd
 wn = t.getscreen()
 t.bgcolor("black")
 
-left_limit = -t.window_width() / 2 + 100
-right_limit = t.window_height() / 2 - 100
-
-top_limit = t.window_height() / 2 - 100
-bottom_limit = -t.window_height() / 2 + 100
-
 game_started = False
 
 caterpillar = t.Turtle()
@@ -35,37 +29,44 @@ text_turtle.write("Press Space to Start", align="center", font=('Arial', 20, "bo
 text_turtle.hideturtle()
 
 score_turtle = t.Turtle()
+score_turtle.color("white")
 score_turtle.hideturtle()
 score_turtle.speed(0)
 
 
 def outsideWindow():
-    x, y = t.pos()
-    return not (left_limit < x < right_limit and bottom_limit < y < top_limit)
+    left_limit = -t.window_width() / 2
+    right_limit = t.window_height() / 2
+
+    top_limit = t.window_height() / 2
+    bottom_limit = -t.window_height() / 2
+    x, y = caterpillar.pos()
+    return left_limit > x or x > right_limit or bottom_limit > y or y > top_limit
 
 
 def placeLeaf():
     leaf.hideturtle()
-    x, y = rd.randint(int(left_limit), int(right_limit)), rd.randint(int(bottom_limit), int(top_limit))
+    x, y = rd.randint(-200, 200), rd.randint(-200, 200)
     leaf.setx(x)
     leaf.sety(y)
     leaf.showturtle()
 
 
-def gameOver():
+def gameOver(current_score):
     caterpillar.hideturtle()
     leaf.hideturtle()
     t.penup()
     t.hideturtle()
-    t.write("Game Over!!!", align="center", font=("Arial", 30, "bold"))
+    t.color("white")
+    t.write("Game Over!!!\nYour Score : " + str(current_score), align="center", font=("Arial", 30, "bold"))
 
 
 def displayScore(current_score):
     score_turtle.clear()
     score_turtle.penup()
     x = t.window_width() / 2 - 50
-    y = t.window_height() / 2 - 50
-    score_turtle.goto(x, y)
+    y = t.window_height() / 2 - 100
+    score_turtle.setpos(x, y)
     score_turtle.pendown()
     score_turtle.write(str(current_score), align="right", font=("Arial", 40, "bold"))
 
@@ -78,50 +79,55 @@ def startGame():
     game_started = True
     text_turtle.clear()
 
-    caterpillar_speed = 2
-    caterpillar_length = 3
-    caterpillar.shapesize(1, caterpillar_length, 1)
+    caterpillar.speed = 2
+    caterpillar.length = 3
+    caterpillar.shapesize(1, caterpillar.length, 1)
+    caterpillar.showturtle()
     current_score = 0
     displayScore(current_score=current_score)
     placeLeaf()
+    # score_turtle.showturtle()
 
     while True:
-        caterpillar.forward(caterpillar_speed)
-        if caterpillar.distance(leaf) < 20:
+        caterpillar.forward(caterpillar.speed)
+        if caterpillar.distance(leaf.pos()) <= 25:
             placeLeaf()
-            caterpillar_length = caterpillar_length + 1
-            caterpillar.shapesize(1, caterpillar_speed, 1)
-            caterpillar_speed += 1
+            caterpillar.length = caterpillar.length + 1
+            caterpillar.shapesize(1, caterpillar.length, 1)
+            caterpillar.speed += 1
             current_score += 10
             displayScore(current_score)
         if outsideWindow():
-            gameOver()
+            gameOver(current_score)
             break
 
 
 def move_up():
     if caterpillar.heading() == 0 or caterpillar.heading() == 180:
-        pass
+        caterpillar.setheading(90)
 
 
 def move_down():
-    pass
+    if caterpillar.heading() == 0 or caterpillar.heading() == 180:
+        caterpillar.setheading(270)
 
 
 def move_right():
-    pass
+    if caterpillar.heading() == 90 or caterpillar.heading() == 270:
+        caterpillar.setheading(0)
 
 
 def move_left():
-    pass
+    if caterpillar.heading() == 90 or caterpillar.heading() == 270:
+        caterpillar.setheading(180)
 
 
 t.onkey(startGame, 'space')
 
-t.onkey(move_up, 'up')
-t.onkey(move_down, 'down')
-t.onkey(move_right, 'right')
-t.onkey(move_left, 'left')
+t.onkey(move_up, 'Up')
+t.onkey(move_down, 'Down')
+t.onkey(move_right, 'Right')
+t.onkey(move_left, 'Left')
 
 t.listen()
 t.mainloop()
